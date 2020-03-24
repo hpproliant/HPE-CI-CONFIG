13670db44583e7fa8222aff047ecd6a414e47d67
proliantci <proliantutils@gmail.com>
2020-03-24 08:03:52 +0000

    crontab cleanup script

---
+++ scripts/clean_logs.py
@@ -0,0 +1,16 @@
+import os
+import shutil
+import time
+
+
+os.system('find /home/ironman-build/hpeproliant.github.io -name job-output.txt > /home/ironman-build/hpeproliant.github.io_job-output')
+with open('/home/ironman-build/hpeproliant.github.io_job-output', 'r') as f:
+    line = f.readline().strip()
+    while(line):
+        if time.time() - os.path.getmtime(line) > 20*24*60*60:
+            print(time.ctime(os.path.getmtime(line)))
+            dir = line.split('job-output.txt')[0]
+            print("Deleting {}".format(dir))
+            shutil.rmtree(dir)
+        line = f.readline().strip()
+
