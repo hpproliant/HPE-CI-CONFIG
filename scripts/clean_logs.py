f60df4003e8ab18f4c829368e5864a8a645d9f1b
proliantci <proliantutils@gmail.com>
2020-04-01 06:20:53 +0000

    change clean_logs to look for job-output.json instead of job-output.txt

--- scripts/clean_logs.py
+++ scripts/clean_logs.py
@@ -9,7 +9,7 @@ with open('/home/ironman-build/hpeproliant.github.io_job-output', 'r') as f:
     while(line):
         if time.time() - os.path.getmtime(line) > 20*24*60*60:
             print(time.ctime(os.path.getmtime(line)))
-            dir = line.split('job-output.txt')[0]
+            dir = line.split('job-output.json')[0]
             print("Deleting {}".format(dir))
             shutil.rmtree(dir)
         line = f.readline().strip()
